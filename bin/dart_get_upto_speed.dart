202fad02ab955de6741cd698d0e96636dca8eccc
mojafa <jmohsa@gmail.com>
2023-05-13 00:35:37 +0300

    initial commit

---
+++ bin/dart_get_upto_speed.dart
@@ -0,0 +1,5 @@
+import 'package:dart_get_upto_speed/dart_get_upto_speed.dart' as dart_get_upto_speed;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_get_upto_speed.calculate()}!');
+}
---
+++ test/dart_get_upto_speed_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_get_upto_speed/dart_get_upto_speed.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
