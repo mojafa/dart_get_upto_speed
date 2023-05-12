1eab9bda4da14067ea54aa3c5353c7a0d253c14c
mojafa <jmohsa@gmail.com>
2023-05-13 00:55:54 +0300

    dart data types

--- bin/dart_get_upto_speed.dart
+++ bin/dart_get_upto_speed.dart
@@ -1,5 +1,26 @@
-import 'package:dart_get_upto_speed/dart_get_upto_speed.dart' as dart_get_upto_speed;
+// import 'package:dart_get_upto_speed/dart_get_upto_speed.dart' as dart_get_upto_speed;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_get_upto_speed.calculate()}!');
+  String name = 'Dart';
+  print(name);
+  print(name.contains('t'));
+
+  int age = 25;
+  print(age.toString());
+  print(age.isEven);
+  double height = 1.75; 
+  print(height.toString());
+  print(height.isNaN);
+
+num myNum = 25;
+print(myNum);
+  bool myBool = true;
+  print(myBool);
+
+  dynamic myDynamic = 'Dart';
+  print(myDynamic);
+  myDynamic = 25;
+    print(myDynamic);
+
+  // no longs and floats in Dart
 }
