3b291e5bedbb22902abec2964543ccc46ffc6520
mojafa <jmohsa@gmail.com>
2023-05-22 20:35:12 +0300

    collection transformations

--- bin/collectionsTransformation.dart
+++ bin/collectionsTransformation.dart
@@ -8,4 +8,22 @@ void main(List<String> arguments){
   print(lengths[0]);
 
   // names.map((name)=>name.length).toList();
-}
+
+
+  final namesFiltered =  names.where((name)=>name.length == 4).toList();
+  print(namesFiltered);
+
+
+
+  for (int i =0; i<namesFiltered.length; i++){
+    print(namesFiltered[i]);
+  }
+
+
+  for (final name in namesFiltered){
+    print(name);
+  }
+
+
+namesFiltered.forEach((name) => print(name));
+} 
