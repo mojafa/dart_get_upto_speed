2a93021322a1bc22defdf24044b8d876f08c66df
mojafa <jmohsa@gmail.com>
2023-05-24 20:06:50 +0300

    freezed unions

--- bin/freezed_unions.dart
+++ bin/freezed_unions.dart
@@ -1,66 +1,64 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+
 part 'FreezedData.freezed.dart';
 
 void main(List<String> arguments) {
   // final myObject = SubClass2() as SuperClass;
- 
-  const resultSuccess =  Result.success(100);
+
+  const resultSuccess = Result.success(100);
   print(
     resultSuccess.when(
       loading: () {
         return 'Loading...';
       },
-      success:(value){
-    return 'Yes! data gotten successfully: $value';
-  }, 
-  failure:(message){
-    return 'Oops! something went wrong: $message';
-  },
-  ),
+      success: (value) {
+        return 'Yes! data gotten successfully: $value';
+      },
+      failure: (message) {
+        return 'Oops! something went wrong: $message';
+      },
+    ),
   );
 
-
-  print(resultSuccess.maybeWhen(
-    orElse: () => '',
-    failure: (message){
-      return 'Oops! something went wrong: $message';
-    },),);
-
+  print(
+    resultSuccess.maybeWhen(
+      orElse: () => '',
+      failure: (message) {
+        return 'Oops! something went wrong: $message';
+      },
+    ),
+  );
 
   print(
     resultSuccess.map(
-      loading: () {
+      loading: (_) {
         return 'Loading...';
       },
-      success:(successCase){
-    return 'Yes! data gotten successfully: ${successCase.value}';
-  }, 
-  failure:(failureCase){
-    return 'Oops! something went wrong: ${failureCase.errorMessage}';
-  },
-  ),
+      success: (successCase) {
+        return 'Yes! data gotten successfully: ${successCase.value}';
+      },
+      failure: (failureCase) {
+        return 'Oops! something went wrong: ${failureCase.errorMessage}';
+      },
+    ),
   );
 
+  print(
+    resultSuccess.maybeMap(
+      orElse: () => '',
+      failure: (message) {
+        return 'Oops! something went wrong: $message';
+      },
+    ),
+  );
+}
 
-
-  print(resultSuccess.maybeMap(
-    orElse: () => '',
-    failure: (message){
-      return 'Oops! something went wrong: $message';
-    },),);
-
-} 
-
-//unions also have named factories as opposed to data classes
 @freezed
 class Result with _$Result {
-  const Result._();
   const factory Result.loading() = _Loading;
   const factory Result.success(int value) = _Success;
   const factory Result.failure(String errorMessage) = _Failure;
 }
-
-
 
 
   //freezed unions check these
