b2e85d05a0383db8eb7fb064826b94fac9b3cc35
mojafa <jmohsa@gmail.com>
2023-05-24 19:30:51 +0300

    bin/files_packages/

---
+++ bin/files_packages/dart_get_up_to_speed.dart
@@ -0,0 +1,10 @@
+part of 'other_file.dart';
+
+void main(List<String> args){}
+
+// ignore: unused_element
+class _PackagePrivateClass{
+  // ignore: unused_field
+  final String _packagePrivateField;
+  _PackagePrivateClass._(this._packagePrivateField)
+}
---
+++ bin/files_packages/other_file.dart
@@ -0,0 +1,5 @@
+part of 'dart_get_upto_speed.dart';
+
+void example(){
+  final packagePrivateClass = _packagePrivateClass._('packagePrivateField');
+  print(packagePrivateClass._packagePrivateField);}
